57ea9c4727849d5227c3c1503a08e3b59d4c13e2
machine.steam@yandex.ru <machine.steam@yandex.ru>
2020-09-07 18:27:03 +0300

    Добавить страницу описание книги

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -7,6 +7,9 @@ namespace Store
     public interface IBookRepository
     {
         Book[] GetAllByIsbn(string isbn);
+
         Book[] GetAllByTitleOrAuthor(string titleOrAuthor);
+
+        Book GetById(int id);
     }
 }
